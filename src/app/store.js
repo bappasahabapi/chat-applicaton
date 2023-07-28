33e413bbf4b99986f6e33d667ff24c0272093e78
bappasahabapi2@gmail.com <bappasahabapi2@gmail.com>
2023-07-28 13:38:45 +0600

    store setup

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { apiSlice } from '../features/api/apiSlice';
+import authSliceReducer from '../features/auth/authSlice'
+import conversationsSliceReducer from '../features/conversations/conversationsSlice'
+import messagesSliceReducer from '../features/messages/messagesSlice'
 
 export const store = configureStore({
   reducer: {
-   
+   [apiSlice.reducerPath]:apiSlice.reducer,
+   auth:authSliceReducer,
+   conversations:conversationsSliceReducer,
+   messages:messagesSliceReducer,
   },
+  devTools:process.env.NODE_ENV !== 'production',
+  middleware:(getDefaultMiddleware) =>getDefaultMiddleware().concat(apiSlice.middleware),
 });
