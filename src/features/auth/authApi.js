f420d469034332395f9f6d3794ddd33aa639fd6a
bappasahabapi2@gmail.com <bappasahabapi2@gmail.com>
2023-07-29 00:22:02 +0600

    [update] authApi

--- src/features/auth/authApi.js
+++ src/features/auth/authApi.js
@@ -1,25 +1,76 @@
 import { apiSlice } from "../api/apiSlice";
+import { userLoggedIn } from "./authSlice";
 
-
-export const authApi=apiSlice.injectEndpoints({
-    endpoints:(builder)=>({
-        register:builder.mutation({
-            query:(data)=>({
-                url:"/register",
-                method:"POST",
-                body:data
-            })
-        }),
-
-        login:builder.mutation({
-            query:(data)=>({
-                url:"/login",
-                method:"POST",
-                body:data
-            })
-        })
+export const authApi = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    register: builder.mutation({
+      query: (data) => ({
+        url: "/register",
+        method: "POST",
+        body: data,
+      }),
     }),
+    async onQueryStarter(arg, { queryFulfilled, dispatch }) {
+      try {
+        const result = await queryFulfilled;
+
+        //first set the login value to  local storage
+        // the dispatch the value to redux store
+        localStorage.setItem(
+          "auth",
+          JSON.stringify({
+            accessToken: result.data.accessToken,
+            user: result.data.user,
+          })
+        );
+
+        //dispatch the value to  the redux store
+        dispatch(
+          userLoggedIn(
+            JSON.stringify({
+              accessToken: result.data.accessToken,
+              user: result.data.user,
+            })
+          )
+        );
+      } catch (error) {}
+    },
+
+    login: builder.mutation({
+      query: (data) => ({
+        url: "/login",
+        method: "POST",
+        body: data,
+      }),
+    }),
+
+    //TODO: same kaj ekaneo hbe
+    async onQueryStarter(arg, { queryFulfilled, dispatch }) {
+      try {
+        const result = await queryFulfilled;
+
+        //first set the login value to  local storage
+        // the dispatch the value to redux store
+        localStorage.setItem(
+          "auth",
+          JSON.stringify({
+            accessToken: result.data.accessToken,
+            user: result.data.user,
+          })
+        );
+
+        //dispatch the value to  the redux store
+        dispatch(
+          userLoggedIn(
+            JSON.stringify({
+              accessToken: result.data.accessToken,
+              user: result.data.user,
+            })
+          )
+        );
+      } catch (error) {}
+    },
+  }),
 });
 
-
-export const { useRegisterMutation, useLoginMutation } =authApi
+export const { useRegisterMutation, useLoginMutation } = authApi;
