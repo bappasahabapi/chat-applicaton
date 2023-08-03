266367229b2ee5b0e6d982d7d4bb3a10fb21c0d7
bappasahabapi2@gmail.com <bappasahabapi2@gmail.com>
2023-08-03 13:17:20 +0600

    [done]-Add authorization header

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,7 +1,18 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-export const apiSlice=  createApi({
-    reducerPath:"api",
-    baseQuery:fetchBaseQuery({baseUrl:process.env.REACT_APP_API_URL}),
-    tagTypes:[],
-    endpoints:(builder)=>({}),
-})
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: process.env.REACT_APP_API_URL,
+    prepareHeaders:async(headers,{getState,endpoint})=>{
+
+        const token =getState()?.auth?.accessToken;
+        if(token){
+            headers.set("Authorization", `Bearer ${token}`);
+        }
+
+        return token;
+    }
+  }),
+  tagTypes: [],
+  endpoints: (builder) => ({}),
+});
