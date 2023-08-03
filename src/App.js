9c8d00bb12a3be40bb8ca856d4942e090d2a38e2
bappasahabapi2@gmail.com <bappasahabapi2@gmail.com>
2023-08-03 16:23:15 +0600

    [done]-08-login persistance

--- src/App.js
+++ src/App.js
@@ -3,18 +3,23 @@ import Conversation from "./pages/Conversation";
 import Inbox from "./pages/Inbox";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import useAuthCheck from "./hooks/useAuthCheck";
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/inbox" element={<Conversation />} />
-                <Route path="/inbox/:id" element={<Inbox />} />
-            </Routes>
-        </Router>
-    );
+  const authChecked = useAuthCheck();
+
+  return !authChecked ? (
+    <div>Checking Authentication ...</div>
+  ) : (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/inbox" element={<Conversation />} />
+        <Route path="/inbox/:id" element={<Inbox />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
