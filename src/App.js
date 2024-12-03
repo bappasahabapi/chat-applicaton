bd8129d5a5a7ba452fa41e9ef0d2440afd5d81d1
Bappa Saha <bappasaha161@gmail.com>
2024-12-03 14:03:09 +0600

    j

--- src/App.js
+++ src/App.js
@@ -3,46 +3,17 @@ import Conversation from "./pages/Conversation";
 import Inbox from "./pages/Inbox";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import useAuthCheck from "./hooks/useAuthCheck";
-import PrivateRoute from "./components/PrivateRoute";
-import PublicRoute from "./components/PublicRoute";
+
 
 function App() {
-  const authChecked = useAuthCheck();
-
-  return !authChecked ? (
-    <div className="text-center font-semibold text-3xl mt-52 ">
-      Checking Authentication ...
-    </div>
-  ) : (
+  return (
     <Router>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <PublicRoute>
-              <Login />
-            </PublicRoute>
-          }
-        />
+        <Route path="/" element={<Login />} />
 
-        <Route
-          path="/register"
-          element={
-            <PublicRoute>
-              <Register />
-            </PublicRoute>
-          }
-        />
+        <Route path="/register" element={<Register />} />
 
-        <Route
-          path="/inbox"
-          element={
-            <PrivateRoute>
-              <Conversation />
-            </PrivateRoute>
-          }
-        />
+        <Route path="/inbox" element={<Conversation />} />
         <Route path="/inbox/:id" element={<Inbox />} />
       </Routes>
     </Router>
